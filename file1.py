06a2b82d92f19d5c4816cec5a866a3fcf2fe24ad
cbawge <cbawge@gmail.com>
2026-06-07 15:31:59 +0530

    Added a sample function

---
+++ file1.py
@@ -0,0 +1,13 @@
+# Generate code for a simple function that adds two numbers
+def add_numbers(a, b):
+    """
+    This function takes two numbers as input and returns their sum.
+    
+    Parameters:
+    a (int or float): The first number to be added.
+    b (int or float): The second number to be added.
+    
+    Returns:
+    int or float: The sum of the two numbers.
+    """
+    return a + b 
